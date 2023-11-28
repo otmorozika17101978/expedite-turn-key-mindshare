5fc7b10a2fb430fa744e12ec28959544b1bef65b
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-11-28 06:19:08 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,29 @@
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+        }
+    }
+}
+        int[] data = new int[10];
+        }
+public class RandomDataGenerator {
+
+        for (int item : data) {
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;}
+    data.forEach(item => {
+
+    return data;
+    const data = generateRandomData();
+    const data = [];
+        console.log("Random Number: " + item);
+    });
+
+    for (let i = 0; i < 10; i++) {
+function main() {
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+function generateRandomData() {
+
+}
